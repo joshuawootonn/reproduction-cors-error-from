8fc2d48d21c6356a4fc148551fab1b8fecdc2be1
joshua wootonn <joshuawootonn@gmail.com>
2024-03-19 11:03:01 -0400

    feat: add title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
+      <h1>Different Origin</h1>
       <button
         onClick={() => {
           fetch("https://test-to-gules.vercel.app/api");
